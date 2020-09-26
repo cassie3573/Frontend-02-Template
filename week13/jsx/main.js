7a1a64b6c772485aa3be6ce7833b246cd30087d5
cassie3573 <zoucaiy@qq.com>
2020-09-27 00:33:12 +0800

    第13周

---
+++ week13/jsx/main.js
@@ -0,0 +1,22 @@
+import {Component, createElement} from "./framework.js"
+import {Carsousel} from "./carousel.js"
+import {Timeline, Animation} from "./animation.js"
+
+let d = [
+	"http://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+	"http://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+	"http://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+	"http://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg"
+]
+
+let a = <Carsousel src={d}/>
+a.mountTo(document.body);
+
+
+let tl = new Timeline();
+window.tl = tl;
+window.animation = new Animation({ set a (v) { console.log(v)}}, "a", 0, 100, 1000, null);
+
+//tl.add(new Animation({ set a (v) { console.log(v)}}, "a", 0, 100, 1000, null));
+tl.start();
+
